1a15f599277da50d3c571d292e0a315df108b849
deedee medina <deedee.medinacorp@gmail.com>
2020-02-19 02:40:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ function whisper(string) {
 return string.toLowerCase()
 }
 
-function logShout(string) {
-}
-console.log(string.toUpperCase());
+const spy = function() {
+  console.log("HELLO");
+};
+
+spy()
