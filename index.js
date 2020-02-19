466fc7af25016dc44a694cffbf3d23470121001e
deedee medina <deedee.medinacorp@gmail.com>
2020-02-19 03:23:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,4 @@ logWhisper()
 const sayHiToGrandma = function(string) {
   if (string.toLowerCase)
   console.log("I can\'t hear you!")
-  else (string.toUpperCase)
-    console.log("YES INDEED!")
 }
